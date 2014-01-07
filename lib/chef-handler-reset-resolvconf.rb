3683375eaacab6f4d289f130198eb8ea5a324d1a
Jake Davis <jake.davis5989@gmail.com>
2014-01-07 23:45:36 +0000

    Fix description of handler

--- lib/chef-handler-reset-resolvconf.rb
+++ lib/chef-handler-reset-resolvconf.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Chef handler to edit MOTD with useful info after a run
+# Copies old resolv.conf to /etc/resolv.conf in the event a Chef run fails
 
 require 'rubygems'
 require 'chef'
